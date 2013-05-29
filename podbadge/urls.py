e758a995abc00ac86c7339841619de528f32cf7e
Flávio Caetano <flavio@vieiracaetano.com>
2013-05-29 15:59:17 -0300

    Redirecting index

--- podbadge/urls.py
+++ podbadge/urls.py
@@ -1,9 +1,11 @@
 from django.conf.urls import patterns, include, url
+from django.views.generic import RedirectView
 
 # Uncomment the next two lines to enable the admin:
 # from django.contrib import admin
 # admin.autodiscover()
 
 urlpatterns = patterns('',
+    url(r'^$', RedirectView.as_view(url='http://fjcaetano.github.io/cocoapod-badges')),
     url(r'^v/(?P<podname>.*?)/badge.png$', 'podbadge.views.badge'),
 )
