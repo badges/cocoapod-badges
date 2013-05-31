7ecb20159d9e5c49766bd32870a57834a279c2fd
Flávio Caetano <flavio@vieiracaetano.com>
2013-05-31 03:20:53 -0300

    Bugfix: serving static files

--- podbadge/settings.py
+++ podbadge/settings.py
@@ -4,7 +4,7 @@
 import os
 PROJECT_ROOT = os.path.dirname(os.path.realpath(__file__))
 
-DEBUG = True
+DEBUG = False
 TEMPLATE_DEBUG = DEBUG
 
 ADMINS = (
@@ -22,7 +22,7 @@ MANAGERS = ADMINS
 
 # Hosts/domain names that are valid for this site; required if DEBUG is False
 # See https://docs.djangoproject.com/en/1.5/ref/settings/#allowed-hosts
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 # Local time zone for this installation. Choices can be found here:
 # http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
@@ -60,7 +60,7 @@ MEDIA_URL = ''
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/var/www/example.com/static/"
-STATIC_ROOT = ''
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static/')
 
 # URL prefix for static files.
 # Example: "http://example.com/static/", "http://static.example.com/"
--- podbadge/urls.py
+++ podbadge/urls.py
@@ -1,5 +1,7 @@
-from django.conf.urls import patterns, include, url
+from django.conf.urls import patterns, url
 from django.views.generic import RedirectView
+
+from django.conf import settings
 
 # Uncomment the next two lines to enable the admin:
 # from django.contrib import admin
@@ -8,4 +10,5 @@ from django.views.generic import RedirectView
 urlpatterns = patterns('',
     url(r'^$', RedirectView.as_view(url='http://fjcaetano.github.io/cocoapod-badges')),
     url(r'^v/(?P<podname>.*?)/badge.png$', 'podbadge.views.badge'),
+    (r'^static/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.STATIC_ROOT}),
 )
