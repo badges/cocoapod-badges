afcee32870e1d92418f7d5d07b2f3b9442431d26
Flávio Caetano <flavio@vieiracaetano.com>
2013-05-31 04:05:33 -0300

    Fixing font

--- podbadge/views.py
+++ podbadge/views.py
@@ -1,43 +1,40 @@
 __author__ = 'flaviocaetano'
 
-from django.http import HttpResponse
+from django.shortcuts import render_to_response
 from django.utils import simplejson
 from django.conf import settings
 from lxml import etree
 
-import os, urllib2, pprint, StringIO
+import os, urllib2, StringIO
 
 def badge(request, podname):
     podname = podname.lower()
-    svg_path = os.path.join(settings.PROJECT_ROOT, 'badge_version.svg')
 
     url = 'http://cocoapods.org/search?query=name:%s&ids=999&offset=0' % podname
 
-    with open(svg_path, 'r') as svg_file:
-        try:
-            response = urllib2.urlopen(url)
-            pod_info = simplejson.loads(response.read())
+    try:
+        response = urllib2.urlopen(url)
+        pod_info = simplejson.loads(response.read())
 
-            allocations = pod_info['allocations'][0]
+        allocations = pod_info['allocations'][0]
 
-            index = 0
-            for result in allocations[4]:
-                name = result.lower()
-                if name == podname:
-                    break
-
-                index += 1
-
-            xml = StringIO.StringIO(allocations[5][index])
-
-            tree = etree.parse(xml)
-            for e in tree.xpath('//span[@class="version"]'):
-                version = e.text.strip()
+        index = 0
+        for result in allocations[4]:
+            name = result.lower()
+            if name == podname:
                 break
 
-            version = version if name.lower() == podname else 'error'
-        except Exception, e:
-            version = 'error'
+            index += 1
 
-        svg_data = svg_file.read() % (version, version)
-        return HttpResponse(svg_data, mimetype="image/svg+xml")
+        xml = StringIO.StringIO(allocations[5][index])
+
+        tree = etree.parse(xml)
+        for e in tree.xpath('//span[@class="version"]'):
+            version = e.text.strip()
+            break
+
+        version = version if name.lower() == podname else 'error'
+    except Exception, e:
+        version = 'error'
+
+    return render_to_response('badge_version.html', {'VERSION':version}, mimetype="image/svg+xml")
